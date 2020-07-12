1f21f45b45ff3be2782f48a9573142965560c9b6
MrAlcoro <alcoro13@gmail.com>
2020-07-12 13:33:25 +0200

    FallenKeys script

---
+++ Assets/RespawnFallenKeys.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KeyboardScripts;
+
+public class RespawnFallenKeys : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        other.collider.transform.position = LetterPool.Instance.transform.position;
+    }
+}
